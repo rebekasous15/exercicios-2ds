46bf893b1d82770f95a3749a45db08b7f5ad2a1b
Rebeka Maria <rebeka.sousa7@aluno.ce.gov.br>
2026-08-05 08:54:03 -0300

    Add files via upload

---
+++ 3-bimestre/orientacao-a-objetos/aula01-revisao-de-objetos-no-javascript-e-introducao-ao-poo/index.js
@@ -0,0 +1,13 @@
+class Filme {
+    constructor(titulo, diretor, ano) {
+        this.titulo = titulo
+        this.diretor = diretor
+        this.ano = ano
+    }
+}
+
+//instâncias 
+const interestelar = new Filme("Interestelar", "Cristopher Nolan", 2014)
+
+//execução
+console.log(interestelar)
