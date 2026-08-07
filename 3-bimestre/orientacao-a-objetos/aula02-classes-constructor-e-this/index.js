89dc065b3fea9098e554cf29f71fae0c6f06c7e8
Rebeka Maria <rebeka.sousa7@aluno.ce.gov.br>
2026-08-07 18:33:52 +0000

    ok

---
+++ 3-bimestre/orientacao-a-objetos/aula02-classes-constructor-e-this/index.js
@@ -0,0 +1,11 @@
+class Funcionario {
+    constructor(nome, cargo, salario){
+        this.nome = nome
+        this.cargo = cargo
+        this.salario = salario
+    }
+}
+
+const Joao = new Funcionario("joao", "desenvolvedor", 5000)
+
+console.log(Joao)
