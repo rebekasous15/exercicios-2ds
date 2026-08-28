4f6fade498e0da514dc57ac08f4e83f79411267b
Rebeka Maria <rebeka.sousa7@aluno.ce.gov.br>
2026-08-28 21:42:37 +0000

    ok

---
+++ 3-bimestre/orientacao-a-objetos/aula12/index.js
@@ -0,0 +1,23 @@
+class Usuario{
+    static quantidade = 0
+    constructor(nome){
+        this.nome = nome
+
+        Usuario.quantidade++
+    }
+
+    static quantidadeUsuarios(){
+        return Usuario.quantidade
+    }
+
+}
+
+const usuario1 = new Usuario("Ana")
+const usuario2 = new Usuario("Carlos")
+const usuario3 = new Usuario("Maria")
+
+console.log(Usuario.quantidadeUsuarios())
+
+console.log(usuario1.nome)
+
+module.exports = {Usuario, usuario1, usuario2, usuario3}
