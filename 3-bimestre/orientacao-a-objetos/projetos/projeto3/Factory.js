9cf44ef43b89f952beaf488fcb6af6f14f10e3e0
Rebeka Maria <rebeka.sousa7@aluno.ce.gov.br>
2026-09-12 11:33:44 -0300

    Implement Factory class for notification creation

--- 3-bimestre/orientacao-a-objetos/projetos/projeto3/Factory.js
+++ 3-bimestre/orientacao-a-objetos/projetos/projeto3/Factory.js
@@ -1,7 +1,25 @@
 const Notificacao = require("./Notificacao")
 // CRIE SUA SOLUÇÃO ABAIXO ================
 
+class Factory{
+    static criar(tipo, mensagem){
 
+        if(tipo === "email"){
+            return new Notificacao.Email(mensagem)
+        }
+
+        if(tipo === "sms"){
+            return new Notificacao.SMS(mensagem)
+        }
+        
+        if(tipo === "app"){
+            return new Notificacao.App(mensagem)
+        }
+
+        throw new Error("Tipo de notificação inválido")
+        
+    }
+}
 
 
 
