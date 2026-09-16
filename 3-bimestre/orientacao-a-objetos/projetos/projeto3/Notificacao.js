20b113c3a712d0c214736739ed2ac40fbf37a06b
Rebeka Maria <rebeka.sousa7@aluno.ce.gov.br>
2026-09-16 07:47:13 -0300

    Update Notificacao.js

--- 3-bimestre/orientacao-a-objetos/projetos/projeto3/Notificacao.js
+++ 3-bimestre/orientacao-a-objetos/projetos/projeto3/Notificacao.js
@@ -29,7 +29,7 @@ class App extends Notificacao{
 
 // === FIM DO CÓDIGO =======================
 // === NÃO FAZER NADA ABAIXO DESSA LINHA ===
-module.exports = Notificacao
+module.exports.Notificacao = Notificacao
 module.exports.Email = Email
 module.exports.SMS = SMS
 module.exports.App = App
