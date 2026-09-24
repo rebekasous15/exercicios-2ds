db5fd298fd06d2bbc873f1039c1999af44ae7d7e
Rebeka Maria <rebeka.sousa7@aluno.ce.gov.br>
2026-09-24 20:30:44 -0300

    Add files via upload

---
+++ 3-bimestre/orientacao-a-objetos/aula16/index.js
@@ -0,0 +1,37 @@
+class Veiculo{
+
+    constructor(marca, modelo){
+        this.marca = marca
+        this.modelo = modelo
+    }
+
+    apresentar(){
+        return `${this.marca} ${this.modelo}`
+    }
+}
+
+class Carro extends Veiculo{
+    constructor(marca, modelo, portas){
+        super(marca, modelo)
+
+        this.portas = portas
+    }   
+
+    apresentar(){
+        return `${this.marca} ${this.modelo}`
+    }
+}
+
+class Moto extends Veiculo{
+    constructor(marca, modelo, cilindrada){
+        super(marca, modelo)
+
+        this.cilindrada = cilindrada
+    }
+}
+
+module.exports = {
+    Veiculo,
+    Carro,
+    Moto
+}
